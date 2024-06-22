26e671961eb23704d1594eb9e23b46738a374799
gopkumr <gopkumr@users.noreply.github.com>
2024-06-22 01:33:33 +0000

    deploy: ee8797be6174669b73af0468efd79a6a3ac4b6db

--- js/theme-mode.js
+++ js/theme-mode.js
@@ -1,11 +1,22 @@
 function switchTheme() {
   const currentStyle = currentTheme();
+  const iconElement = document.getElementById('github-icon');
 
-  if (currentStyle == 'light') {
+  if (currentStyle === 'light') {
     setTheme('dark');
+    setIconTheme('dark')
+    if (iconElement) {
+      iconElement.setAttribute('class', 'octicon');
+      iconElement.setAttribute('color', '#f0f6fc');
+    }
   }
   else {
     setTheme('light');
+    setIconTheme('light')
+    if (iconElement) {
+      iconElement.removeAttribute('color');
+      iconElement.removeAttribute('class');
+    }
   }
 }
 
@@ -15,6 +26,15 @@ function setTheme(style) {
   });
   document.documentElement.setAttribute('data-color-mode', style);
   localStorage.setItem('data-color-mode', style);
+}
+
+function setIconTheme(theme) {
+  twitterIconElement = document.getElementById('twitter-icon');
+  if (theme === 'light') {
+    twitterIconElement.setAttribute("fill", "black")
+  } else if (theme === 'dark') {
+    twitterIconElement.setAttribute("fill", "white")
+  }
 }
 
 function currentTheme() {
